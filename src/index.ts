0f07aa96d3bbb541777c8f3552a55b4d070e11b0
Amanatri0 <atriaman8897@gmail.com>
2025-07-27 15:00:40 +0530

    Cors changes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,26 @@ dotenv.config();
 import express from "express";
 import { userRoutes } from "./routes/user/userRoutes";
 import { adminRoutes } from "./routes/admin/adminRoute";
+import cors from "cors";
 
 const port = process.env.PORT;
 const app = express();
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin like mobile apps or curl requests
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
